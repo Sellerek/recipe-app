1f4b90703b2ff98e907f0f32095582d7ca388ca5
Krzysztof Pietruszka <pietruszkak@gmail.com>
2019-07-25 22:35:21 +0200

    Updates

---
+++ precache-manifest.8f6ffe531539f82d4a468c8f6e11a705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab8e69466ba1ceb27e39f2dc170d01c",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "85d27a21a176932ad274",
+    "url": "/recipe-app/static/css/main.094df325.chunk.css"
+  },
+  {
+    "revision": "458f95c40dc6e34bddb7",
+    "url": "/recipe-app/static/js/2.efc19865.chunk.js"
+  },
+  {
+    "revision": "85d27a21a176932ad274",
+    "url": "/recipe-app/static/js/main.f04c54ef.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  },
+  {
+    "revision": "8712936cd6e1cd29ebdfe9c6dfb4acef",
+    "url": "/recipe-app/static/media/food.8712936c.jpg"
+  }
+]);
